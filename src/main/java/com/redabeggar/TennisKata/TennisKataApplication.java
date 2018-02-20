4d94957b672db07543d6d14d17ea7565b354d941
Reda BEGGAR <reda.beggar@corp.talan.com>
2018-02-20 15:47:06 +0100

    create spring application -spring initialzr-

---
+++ src/main/java/com/redabeggar/TennisKata/TennisKataApplication.java
@@ -0,0 +1,12 @@
+package com.redabeggar.TennisKata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisKataApplication.class, args);
+	}
+}
